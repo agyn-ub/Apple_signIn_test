11ee76f07b4822e4c4e142eb24ce61951c5933ca
agyn-ub <agyn.bolatov.1992@gmail.com>
2025-07-30 22:51:24 +0500

    Initial Commit

---
+++ Apple_signIn_test/Apple_signIn_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_signIn_testApp.swift
+//  Apple_signIn_test
+//
+//  Created by Agyn Bolatov on 30.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_signIn_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple_signIn_test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple_signIn_test
+//
+//  Created by Agyn Bolatov on 30.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
